68b37fcb062e312b4f3bc48550713bb9fbad48ed
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 17:10:31 -0700

    complete student creation form

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,9 +9,44 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    html = render_template("student_info.html", 
+                           first=first, 
+                           last=last,
+                           github=github)
+
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/student-add-form")
+def display_student_form():
+    """Display student form."""
+
+    return render_template("student_add.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student via form."""
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+    link_string = "/student?github=" + github
+
+    return render_template("student_add_confirmation.html",
+                            first_name=first_name,
+                            last_name=last_name,
+                            github=github,
+                            link_string=link_string)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
